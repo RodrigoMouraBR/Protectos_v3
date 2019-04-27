b7aaad35b835464e376363c9b45ce1b9e3cf2a38
ma4cio <ma4cio@gmail.com>
2019-04-27 17:42:49 -0300

    Correção do .csproj que estava dando erro no ao buscar a pasta packeges e .gitignore que ignora a migrations

---
+++ src/Protectos.Web/Controllers/AdministradoraController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Protectos.Web.Controllers
+{
+    public class AdministradoraController : Controller
+    {
+        // GET: Administradora
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
