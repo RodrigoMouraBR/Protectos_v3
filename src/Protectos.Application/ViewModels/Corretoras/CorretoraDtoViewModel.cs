f5c0a04267fdc8c59129c406fe7231d3d2012e13
Rodrigo de Moura <rodrigomoura.developer@gmail.com>
2019-04-30 01:54:05 -0300

    Implementando AutoMapper, IunitOfWork na camada de application

---
+++ src/Protectos.Application/ViewModels/Corretoras/CorretoraDtoViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Protectos.Application.ViewModels.Corretoras
+{
+    public class CorretoraDtoViewModel
+    {
+        public CorretoraDtoViewModel(CorretoraViewModel corretora, CorretoraEnderecoViewModel endereco, CorretoraEmailViewModel email, CorretoraTelefoneViewModel telefone)
+        {
+            Corretora = corretora;
+            Endereco = endereco;
+            Email = email;
+            Telefone = telefone;
+        }
+        protected CorretoraDtoViewModel()
+        {
+        }
+        public CorretoraViewModel Corretora { get; private set; }
+        public CorretoraEnderecoViewModel Endereco { get; private set; }
+        public CorretoraEmailViewModel Email { get; private set; }
+        public CorretoraTelefoneViewModel Telefone { get; private set; }
+    }
+}
---
+++ src/Protectos.Application/ViewModels/Operadoras/OperadoraDtoViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Protectos.Application.ViewModels.Operadoras
+{
+    public class OperadoraDtoViewModel
+    {
+        public OperadoraDtoViewModel(
+                                     OperadoraViewModel operadora, 
+                                     OperadoraEnderecoViewModel endereco, 
+                                     OperadoraEmailViewModel email, 
+                                     OperadoraTelefoneViewModel telefone
+                                    )
+        {
+            Operadora = operadora;
+            Endereco = endereco;
+            Email = email;
+            Telefone = telefone;
+        }
+        protected OperadoraDtoViewModel()
+        {
+        }
+        public OperadoraViewModel Operadora { get; private set; }
+        public OperadoraEnderecoViewModel Endereco { get; private set; }
+        public OperadoraEmailViewModel Email { get; private set; }
+        public OperadoraTelefoneViewModel Telefone { get; private set; }
+    }
+}
