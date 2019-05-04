cb79ce362794f6f3f3ceab8c046721edb69d2e39
ma4cio <ma4cio@gmail.com>
2019-05-04 09:55:25 -0300

    Camada de  apresentacao - criação e manutenção das já existentes

---
+++ src/Protectos.Web/Controllers/BeneficiarioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Protectos.Web.Controllers
+{
+    public class BeneficiarioController : Controller
+    {
+        // GET: Beneficiario
+        public ActionResult Listar()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
