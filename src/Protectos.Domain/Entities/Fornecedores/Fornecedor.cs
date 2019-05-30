7ee0fd3541514eb67ad84acb3833a722956d6b2d
Rodrigo de Moura <rodrigomoura.developer@gmail.com>
2019-05-29 22:21:56 -0300

    Ajustando tsconfig do typescript

--- src/Protectos.Domain/Entities/Fornecedores/Fornecedor.cs
+++ src/Protectos.Domain/Entities/Fornecedores/Fornecedor.cs
@@ -1,14 +1,46 @@
 ﻿using Protectos.Domain.Core.Models;
+using Protectos.Domain.ValuesObjects;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Protectos.Domain.Entities.Fornecedores
 {
     public class Fornecedor : Entity<Fornecedor>
     {
+        protected Fornecedor()
+        {
+
+        }
+        public string RazaoSocial { get; private set; }
+        public string NomeFantasia { get; private set; }
+        public string InscricaoEstadual { get; private set; }
+        public string InscricaoMunicipal { get; private set; }
+        public CNPJ Cnpj { get; private set; }
+        
+        public string Site { get; private set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         public override bool IsValid()
         {
             throw new NotImplementedException();
