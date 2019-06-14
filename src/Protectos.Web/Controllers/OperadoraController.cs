2f4502770624d51b13de5ca30179f251907b1016
Marcio <ma4cio@gmail.com>
2019-06-14 01:44:33 -0300

    Controller Operaradas/Entidades/Corretoras e correções no cadastro da adminstradora

--- src/Protectos.Web/Controllers/OperadoraController.cs
+++ src/Protectos.Web/Controllers/OperadoraController.cs
@@ -2,6 +2,7 @@
 using Protectos.Application.ViewModels.Operadoras;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,66 +14,86 @@ namespace Protectos.Web.Controllers
 
     public class OperadoraController : Controller
     {
+        private readonly string caminhoImagens = ConfigurationManager.AppSettings["Imagens"];
+
         private readonly IOperadoraApplicationService _operadoraApplicationService;
+
         public OperadoraController(IOperadoraApplicationService operadoraApplicationService)
         {
             _operadoraApplicationService = operadoraApplicationService;
         }
         public ActionResult Index()
         {
-            return View(_operadoraApplicationService.OperadoraObterTodos());
+            return View(_operadoraApplicationService.OperadoraObterAtivo());
         }
-        public ActionResult Details(Guid? id)
+        public ActionResult Detalhe(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var OperadoraViewModel = _operadoraApplicationService.OperadoraObterPorId(id.Value);
-            if (OperadoraViewModel == null)
+            var operadoraViewModel = _operadoraApplicationService.OperadoraObterPorId(id.Value);
+            if (operadoraViewModel == null)
             {
                 return HttpNotFound();
             }
-            return View(OperadoraViewModel);
+
+            return PartialView("_Detalhe", operadoraViewModel);
         }
-        public ActionResult Create()
+        public ActionResult Incluir()
         {
             return View();
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(OperadoraViewModel OperadoraViewModel)
+        public ActionResult Incluir(OperadoraViewModel operadoraViewModel)
         {
+
             if (ModelState.IsValid)
             {
-                _operadoraApplicationService.OperadoraAdicionar(OperadoraViewModel);
+                var operadora = _operadoraApplicationService.OperadoraAdicionar(operadoraViewModel);
+
+                if (!operadora.ValidationResult.IsValid)
+                {
+                    foreach (var erro in operadora.ValidationResult.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, erro.ErrorMessage);
+                    }
+                    return View(operadora);
+                }
+                if (operadora.ValidationResult.ToString() != string.Empty)
+                {
+                    ViewBag.Sucesso = operadora.ValidationResult.Errors.ToList();
+                    return View(operadora);
+                }
                 return RedirectToAction("Index");
             }
-            return View(OperadoraViewModel);
+            return View(operadoraViewModel);
         }
-        public ActionResult Edit(Guid? id)
+        public ActionResult Editar(Guid? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            OperadoraViewModel OperadoraViewModel = _operadoraApplicationService.OperadoraObterPorId(id.Value);
-            if (OperadoraViewModel == null)
+            var operadoraViewModel = _operadoraApplicationService.OperadoraObterPorId(id.Value);
+            if (operadoraViewModel == null)
             {
                 return HttpNotFound();
             }
-            return View(OperadoraViewModel);
+            ViewBag.OperadoraId = id;
+            return View(operadoraViewModel);
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(OperadoraViewModel OperadoraViewModel)
+        public ActionResult Editar(OperadoraViewModel operadoraViewModel)
         {
             if (ModelState.IsValid)
             {
-                _operadoraApplicationService.OperadoraAdicionar(OperadoraViewModel);
+                _operadoraApplicationService.OperadoraAtualizar(operadoraViewModel);
                 return RedirectToAction("Index");
             }
-            return View(OperadoraViewModel);
+            return View(operadoraViewModel);
         }
         public ActionResult Delete(Guid? id)
         {
@@ -80,12 +101,12 @@ namespace Protectos.Web.Controllers
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            var OperadoraViewModel = _operadoraApplicationService.OperadoraObterPorId(id.Value);
-            if (OperadoraViewModel == null)
+            var operadoraViewModel = _operadoraApplicationService.OperadoraObterPorId(id.Value);
+            if (operadoraViewModel == null)
             {
                 return HttpNotFound();
             }
-            return View(OperadoraViewModel);
+            return View(operadoraViewModel);
         }
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -102,15 +123,16 @@ namespace Protectos.Web.Controllers
             }
             base.Dispose(disposing);
         }
+
         //Endereco       
         public ActionResult ListarEnderecos(Guid id)
         {
-            ViewBag.AdministradoraId = id;
+            ViewBag.OperadoraId = id;
             return PartialView("_EnderecosList", _operadoraApplicationService.OperadoraObterPorId(id).Enderecos);
         }
         public ActionResult AdicionarEndereco(Guid id)
         {
-            ViewBag.AdministradoraId = id;
+            ViewBag.OperadoraId = id;
             return PartialView("_AdicionarEndereco");
         }
         [HttpPost]
@@ -126,12 +148,12 @@ namespace Protectos.Web.Controllers
 
             return PartialView("_AdicionarEndereco", enderecoViewModel);
         }
-        public ActionResult AtualizarEndereco(Guid id)
-        {
-            //TODO : FALTA OBTER ENDERECO POR ID DA OPERADORA
-            //   return PartialView("_AtualizarEndereco", _operadoraApplicationService.OperadoraEnderecoObterTodos(id));
-            return PartialView("_AtualizarEndereco", null);
-        }
+        //TODO IMPLEMENTAR _operadoraApplicationService.OperadoraEnderecoObterPorId
+        //public ActionResult AtualizarEndereco(Guid id)
+        //{
+        //    return PartialView("_AtualizarEndereco", _operadoraApplicationService.OperadoraEnderecoObterPorId(id));
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult AtualizarEndereco(OperadoraEnderecoViewModel enderecoViewModel)
@@ -143,16 +165,17 @@ namespace Protectos.Web.Controllers
                 string url = Url.Action("ListarEnderecos", "Operadora", new { id = enderecoViewModel.OperadoraId });
                 return Json(new { success = true, url = url });
             }
+
             return PartialView("_AtualizarEndereco", enderecoViewModel);
         }
-
+        ////TODO IMPLEMENTAR _operadoraApplicationService.OperadoraEnderecoObterPorId
         //public ActionResult DeletarEndereco(Guid? id)
         //{
         //    if (id == null)
         //    {
         //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
         //    }
-            
+
         //    var enderecoViewModel = _operadoraApplicationService.OperadoraEnderecoObterPorId(id.Value);
         //    if (enderecoViewModel == null)
         //    {
@@ -160,23 +183,93 @@ namespace Protectos.Web.Controllers
         //    }
         //    return PartialView("_DeletarEndereco", enderecoViewModel);
         //}
-
-
         //[HttpPost, ActionName("DeletarEndereco")]
         //[ValidateAntiForgeryToken]
         //public ActionResult DeletarEnderecoConfirmed(Guid id)
         //{
-        //    var administradoraId = _OperadoraApplicationService.OperadoraEnderecoObterPorId(id).AdministradoraId;
-        //    _OperadoraApplicationService.DeleteOperadoraEndereco(id);
-        //    string url = Url.Action("ListarEnderecos", "Administradora", new { id = administradoraId });
+        //    var operadoraId = _operadoraApplicationService.OperadoraEnderecoObterPorId(id).OperadoraId;
+        //    _operadoraApplicationService.DeleteOperadoraEndereco(id);
+        //    string url = Url.Action("ListarEnderecos", "Operadora", new { id = operadoraId });
         //    return Json(new { success = true, url = url });
         //}
 
-        public ActionResult ObterImagemOperadora(Guid id)
+
+        //Email       
+        public ActionResult ListarEmails(Guid id)
         {
-            var root = @"C:\Users\rodri\Pictures\Spartacus\";
-            var foto = Directory.GetFiles(root, id + "*").FirstOrDefault();
-            if (foto != null && !foto.StartsWith(root))
+            ViewBag.OperadoraId = id;
+            return PartialView("_EmailsList", _operadoraApplicationService.OperadoraObterPorId(id).Emails);
+        }
+        public ActionResult AdicionarEmail(Guid id)
+        {
+            ViewBag.OperadoraId = id;
+            return PartialView("_AdicionarEmail");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AdicionarEmail(OperadoraEmailViewModel emailViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _operadoraApplicationService.OperadoraEmailAdicionar(emailViewModel);
+                string url = Url.Action("ListarEmails", "Operadora", new { id = emailViewModel.OperadoraId });
+                return Json(new { success = true, url = url });
+            }
+
+            return PartialView("_AdicionarEmail", emailViewModel);
+        }
+        //TODO IMPLEMENTAR _operadoraApplicationService.OperadoraEnderecoObterPorId
+        //public ActionResult AtualizarEmail(Guid id)
+        //{
+        //    return PartialView("_AtualizarEmail", _operadoraApplicationService.OperadoraEmailObterPorId(id));
+        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AtualizarEmail(OperadoraEmailViewModel emailViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _operadoraApplicationService.OperadoraEmailAtualizar(emailViewModel);
+
+                string url = Url.Action("ListarEmails", "Operadora", new { id = emailViewModel.OperadoraId });
+                return Json(new { success = true, url = url });
+            }
+
+            return PartialView("_AtualizarEmail", emailViewModel);
+        }
+        //TODO IMPLEMENTAR _operadoraApplicationService.OperadoraEnderecoObterPorId
+        //public ActionResult DeletarEmail(Guid? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+
+        //    var emailViewModel = _operadoraApplicationService.OperadoraEmailObterPorId(id.Value);
+        //    if (emailViewModel == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return PartialView("_DeletarEmail", emailViewModel);
+        //}
+        //[HttpPost, ActionName("DeletarEmail")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeletarEmailConfirmed(Guid id)
+        //{
+        //    var operadoraId = _operadoraApplicationService.OperadoraEmailObterPorId(id).OperadoraId;
+        //    _operadoraApplicationService.DeleteOperadoraEmail(id);
+        //    string url = Url.Action("ListarEmails", "Operadora", new { id = operadoraId });
+        //    return Json(new { success = true, url = url });
+        //}
+
+
+
+
+        public ActionResult ObterImagemCliente(Guid id)
+        {
+            //var root = caminhoImagens;
+            var foto = Directory.GetFiles(caminhoImagens, id + "*").FirstOrDefault();
+            if (foto != null && !foto.StartsWith(caminhoImagens))
             {
                 // Validando qualquer acesso indevido além da pasta permitida
                 throw new HttpException(403, "Acesso Negado");
